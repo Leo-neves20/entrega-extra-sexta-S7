8247146791bd4b57fdbcb7a08a0ee1de71c0d018
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-07-15 14:24:41 -0300

    primeiro commit

---
+++ anagrams1.js
@@ -0,0 +1,50 @@
+
+
+
+const result = []
+let body = document.querySelector('body')
+
+function getAnagramsOf(input){
+    
+    result.push(input)
+
+}
+
+const button = document.getElementById("findButton");
+button.addEventListener("click", function () {
+
+   const word = [] 
+   let typedText = document.getElementById("input").value;
+   word.push(typedText)
+     
+    for(let i = 0; i < palavras.length; i++){
+
+        
+        let palavrasFormatada = alphabetize(palavras[i])
+        let wordFormatada     = alphabetize(word[0])
+
+        if(palavrasFormatada == wordFormatada){
+
+            getAnagramsOf(palavras[i])
+
+        }
+
+    }
+
+    let textContent = document.createElement("p")
+    textContent.innerText = result
+    
+    return body.appendChild(textContent)
+
+});
+
+function alphabetize(a){
+
+    return a.toLowerCase().split("").sort().join("").trim()
+
+}
+
+
+
+
+
