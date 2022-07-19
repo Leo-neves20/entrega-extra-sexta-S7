06954f4b6f477c28d247a75157ee32ac9e31a34d
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-07-19 15:00:25 -0300

    resolvi a repetição de palavras

--- anagrams1.js
+++ anagrams1.js
@@ -1,43 +1,39 @@
 
 
 
-const result = []
+
 let body = document.querySelector('body')
+let textContent = document.createElement("p")
 
 function getAnagramsOf(input){
-    
-    result.push(input)
 
-}
+const result = []
 
-const button = document.getElementById("findButton");
-button.addEventListener("click", function () {
-
-   const word = []
-
-   let typedText = document.getElementById("input").value;
-   word.push(typedText)
-     
     for(let i = 0; i < palavras.length; i++){
 
         
-        let palavrasFormatada = alphabetize(palavras[i])
-        let wordFormatada     = alphabetize(word[0])
+        let palavrasFormatada = alphabetize(input)
+        let wordFormatada     = alphabetize(palavras[i])
 
         if(palavrasFormatada == wordFormatada){
 
-            getAnagramsOf(palavras[i])
+            result.push(palavras[i])
 
         }
 
     }
 
-    let arrayStrnig = result.toString().replace(/,/g," ")
+    textContent.innerText = result
+    return result
+    
+}
 
-    let textContent = document.createElement("p")
-    textContent.innerText = arrayStrnig 
-    return body.appendChild(textContent)
-
+const button = document.getElementById("findButton");
+button.addEventListener("click", function () {
+  
+   let typedText = document.getElementById("input").value;
+   getAnagramsOf(typedText)
+     
 });
 
 function alphabetize(a){
@@ -45,6 +41,8 @@ function alphabetize(a){
     return a.toLowerCase().split("").sort().join("").trim()
 
 }
+
+body.appendChild(textContent)
 
 
 
